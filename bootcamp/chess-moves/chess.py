f7c79689135e87020e9e54fc5f904faa5213e8bb
wethinkcode <wtc@student.wethinkcode.co.za>
2026-07-23 15:55:12 +0200

    Pseudocode to undestand generate_moves

--- bootcamp/chess-moves/chess.py
+++ bootcamp/chess-moves/chess.py
@@ -1,20 +1,31 @@
-def parse_fen(fen):
+def parse_fen(fen): # INPUT "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     fen_pieces, to_move, castling_rights, ep, hm, fm = fen.split(" ")
     pieces = [[]]
     for char in fen:
-        if char.isdigit():
-            pieces[-1].extend(["."] * int(char))
+        if char.isdigit(): # checks if the character is a digit
+            pieces[-1].extend(["."] * int(char)) # makes  a list of . 
         elif char == "/":
             pieces.append([])
         else:
             pieces[-1].append(char)
 
-    return ...
+    return pieces # OUTPUT [[r, n, b, q, k, b, n, r], [p, p, p,...], [.,.,.,.,]]
 
 
 def generate_moves(board):
     raise NotImplementedError("This function is not implemented yet.")
 
+#  if  that char is w or b: "deciding which pieces need to be moved"
+# move either
+# for each piece can that piece move?
+#lOOP if not, move to the next piece (continue)
+# increase number of possible moves
+
+
+# OUTPUT 
+
 
 def apply_move(board, move):
     raise NotImplementedError("This function is not implemented yet.")
+
+print(parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
