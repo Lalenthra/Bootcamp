4db9f06c3219ead5c04c2d708cbf542f7716c2b2
Lalenthra <79930419+Lalenthra@users.noreply.github.com>
2026-07-23 21:33:56 +0200

    Changed fen to fen_pieces, added comments to code

--- bootcamp/chess-moves/chess.py
+++ bootcamp/chess-moves/chess.py
@@ -1,7 +1,7 @@
 def parse_fen(fen): # INPUT "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     fen_pieces, to_move, castling_rights, ep, hm, fm = fen.split(" ")
     pieces = [[]]
-    for char in fen:
+    for char in fen_pieces: # only takes out the board
         if char.isdigit(): # checks if the character is a digit
             pieces[-1].extend(["."] * int(char)) # makes  a list of . 
         elif char == "/":
@@ -13,19 +13,24 @@ def parse_fen(fen): # INPUT "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq 
 
 
 def generate_moves(board):
-    raise NotImplementedError("This function is not implemented yet.")
+    # raise NotImplementedError("This function is not implemented yet.")
+    moves = 0
+    # function that moves pawn move_pawn()
+         
+    # function that moves knight move_pawn()
+    # 
 
-#  if  that char is w or b: "deciding which pieces need to be moved"
+# OUTPUT integer with number of possible moves
+    return moves
+
+
+def apply_move(board, move):
+    raise NotImplementedError("This function is not implemented yet.")
+    #  if  that char is w or b: "deciding which pieces need to be moved"
 # move either
 # for each piece can that piece move?
 #lOOP if not, move to the next piece (continue)
 # increase number of possible moves
 
-
-# OUTPUT 
-
-
-def apply_move(board, move):
-    raise NotImplementedError("This function is not implemented yet.")
 
 print(parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
