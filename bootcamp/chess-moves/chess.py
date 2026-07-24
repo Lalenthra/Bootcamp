a038903b8ec48d43f912bbaa555b33f904b8be2a
Lalenthra <79930419+Lalenthra@users.noreply.github.com>
2026-07-24 07:49:14 +0200

    Added function create board to check output of parse_fen

--- bootcamp/chess-moves/chess.py
+++ bootcamp/chess-moves/chess.py
@@ -10,6 +10,15 @@ def parse_fen(fen): # INPUT "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq 
             pieces[-1].append(char)
 
     return pieces # OUTPUT [[r, n, b, q, k, b, n, r], [p, p, p,...], [.,.,.,.,]]
+def print_board(fen):
+    # check output of parse_fen is producing the right thing
+    board = parse_fen(fen)
+    for i , row in enumerate(board):
+        rank = 8 - i
+        print(rank, ' ', ' '.join(row))
+    print(('    a b c d e f g h'))
+    
+print_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
 
 def generate_moves(board):
@@ -18,7 +27,22 @@ def generate_moves(board):
     # function that moves pawn move_pawn()
          
     # function that moves knight move_pawn()
-    # 
+    
+    ## looping through the board, finding pieces to move
+    # for i in range(len(board)): # for each row
+        # for j in range(len(board[i])): # for each block
+        #   if board[i][j] == '.'
+                #continue # if empty,
+            # elif board[i][j] == 'p' or board[i][j] == 'P':
+            #   #call on the function that calc how many moves a pawn has
+            #   # moves +=1
+            # elif board[i][j] == 'n' or board[i][j] == 'N':
+            #   #call on the function that calc how many moves a knight has
+            #   # moves +=1
+             # elif board[i][j] == '' or board[i][j] == 
+            #   #call on the function that calc how many moves a pawn has
+
+
 
 # OUTPUT integer with number of possible moves
     return moves
